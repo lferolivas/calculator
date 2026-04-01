51a2a0c26e45950a22eff687a0abd6c86d8314f2
lferolivas <lfer.olivas@gmail.com>
2026-03-31 21:48:06 -0600

    test de redeploy

--- src/main/java/com/calculator/CalculatorServlet.java
+++ src/main/java/com/calculator/CalculatorServlet.java
@@ -17,6 +17,7 @@ public class CalculatorServlet extends HttpServlet {
             case "mul": result = a * b; break;
             case "div": if(b != 0) result = a / b; else error = "Error: division por cero"; break;
         }
+        // test
         res.setContentType("text/html");
         PrintWriter out = res.getWriter();
         out.println("<h1>Resultado: " + (error.isEmpty() ? result : error) + "</h1>");
